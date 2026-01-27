6024fec788f2bc17eb3b933ad8be28a632fd8aa7
RPPombo <rodrigopanisipombo@gmail.com>
2026-01-27 18:35:51 -0300

    Continunado o curso de C++

--- C++/Curso 1/JogoDaAdivinhacao.cpp
+++ C++/Curso 1/JogoDaAdivinhacao.cpp
@@ -4,7 +4,34 @@ using namespace std; // utilizado para indicar que está usando varias funções
 int main(){
     cout << "Bem-Vindo ao jogo de Adivinhação!" << endl; // É dessa forma que é impresso strings no terminal. O std::endl serve como um \n
 
-    int numero_secreto = 42;
+    const int NUMERO_SECRETO = 42; // manter a váriavel sempre a mesma com const
 
-    cout << "O número secreto é "<< numero_secreto << ". Não conte para ninguém!" << endl;
+    int tentativas = 0;
+
+    bool acertou = false;
+
+    while (!acertou){
+        tentativas ++;
+        int chute;
+        cout << "Tentativa " << tentativas << endl;
+
+        cout << "Qual o valor do chute? ";
+        cin >> chute; // Maneira de receber um input
+
+        cout << "Seu chute foi: " << chute << endl;
+
+        acertou = chute == NUMERO_SECRETO;
+        bool menor = chute < NUMERO_SECRETO;
+
+        if (acertou) {
+            cout << "Parabéns você acertou!" << endl;
+            cout << "Foram " << tentativas << " tentativas utilizadas!" << endl;
+        } else if (menor) {
+            cout << "Seu chute foi menor que o número secreto!" << endl;
+        } else {
+            cout << "Seu chute foi maior que o número secreto!" << endl;
+        }
+    }
+
+    cout << "Fim de jogo!" << endl;
 }
