d28544d4abda265285cc0feacd19297e3e717a1a
RPPombo <rodrigopanisipombo@gmail.com>
2026-01-28 15:36:18 -0300

    Colocando a pontuação no jogo e aprendendo a colocar precisão e fixar casas decimais

--- C++/Curso 1/JogoDaAdivinhacao.cpp
+++ C++/Curso 1/JogoDaAdivinhacao.cpp
@@ -7,6 +7,7 @@ int main(){
     const int NUMERO_SECRETO = 42; // manter a váriavel sempre a mesma com const
 
     int tentativas = 0;
+    double pontos = 1000.0;
 
     bool acertou = false;
 
@@ -19,6 +20,10 @@ int main(){
         cin >> chute; // Maneira de receber um input
 
         cout << "Seu chute foi: " << chute << endl;
+
+        double pontos_perdidos = abs(chute - NUMERO_SECRETO)/2.0; // Quando uma conta é feita apenas com valores inteiros o valor de saída sempre será inteiro, por isso o 2.0
+
+        pontos -= pontos_perdidos;
 
         acertou = chute == NUMERO_SECRETO;
         bool menor = chute < NUMERO_SECRETO;
@@ -33,5 +38,8 @@ int main(){
         }
     }
 
+    cout.precision(2); // Responsável por colocar precisão nas váriaveis numéricas do terminal 
+    cout << fixed; // Responsável por fixar as casas decimais no mesmo lugar
+    cout << "Sua pontuação foi de " << pontos << " pontos." << endl;
     cout << "Fim de jogo!" << endl;
 }
