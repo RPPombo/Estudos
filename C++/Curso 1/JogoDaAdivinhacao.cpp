fc8c1540c32e27d06e638aa33816ec6c1ad4a16c
RPPombo <rodrigopanisipombo@gmail.com>
2026-01-29 14:50:56 -0300

    Utilizando mais algumas bibliotecas, criando números aleatórios e definindo dificuldades pro jogo (C++)

--- C++/Curso 1/JogoDaAdivinhacao.cpp
+++ C++/Curso 1/JogoDaAdivinhacao.cpp
@@ -1,20 +1,41 @@
 #include <iostream> // Biblioteca padrão para entrada e saída em C++
+#include <cctype> // Biblioteca para transformações em chars
+#include <cstdlib> // Biblioteca padrão de C
+#include <ctime> // Bilioteca para o receber o tempo
 using namespace std; // utilizado para indicar que está usando varias funções do std, tirando assim a necessidade de ficar colocando toda hora std::
 
 int main(){
-    cout << "Bem-Vindo ao jogo de Adivinhação!" << endl; // É dessa forma que é impresso strings no terminal. O std::endl serve como um \n
+    cout << "Bem-Vindo ao jogo de Adivinhação!" << endl;// É dessa forma que é impresso strings no terminal. O std::endl serve como um \n
 
-    const int NUMERO_SECRETO = 42; // manter a váriavel sempre a mesma com const
+    cout << "Escolha o nível de dificuldade:" << endl;
+    cout << "(F)Fácil | (M) Médio | (D) Difícil" << endl;
 
-    int tentativas = 0;
+    char dificuldade;
+    cin >> dificuldade;
+    dificuldade = toupper(dificuldade); // deixando a entrada sempre em uppercase
+
+    int total_tentativas;
+
+    if (dificuldade == 'F' ){
+        total_tentativas = 15; 
+    } else if (dificuldade == 'M') {
+        total_tentativas = 10;
+    } else {
+        total_tentativas = 5;
+    }
+
+    srand(time(NULL));
+    const int NUMERO_SECRETO = rand() % 100; // manter a váriavel sempre a mesma com const
+
+    int tentativas = 1;
     double pontos = 1000.0;
 
     bool acertou = false;
 
-    while (!acertou){
-        tentativas ++;
+    while (!acertou && tentativas <= total_tentativas){
+        
         int chute;
-        cout << "Tentativa " << tentativas << endl;
+        cout << "Tentativa " << tentativas << " de " << total_tentativas << endl;
 
         cout << "Qual o valor do chute? ";
         cin >> chute; // Maneira de receber um input
@@ -29,17 +50,23 @@ int main(){
         bool menor = chute < NUMERO_SECRETO;
 
         if (acertou) {
-            cout << "Parabéns você acertou!" << endl;
-            cout << "Foram " << tentativas << " tentativas utilizadas!" << endl;
+            cout << "\nParabéns você acertou!" << endl;
+            cout << "\nForam " << tentativas << " tentativas utilizadas!" << endl;
+            cout.precision(2); // Responsável por colocar precisão nas váriaveis numéricas do terminal 
+            cout << fixed; // Responsável por fixar as casas decimais no mesmo lugar
+            cout << "Sua pontuação foi de " << pontos << " pontos." << endl;
         } else if (menor) {
             cout << "Seu chute foi menor que o número secreto!" << endl;
+            tentativas ++;
         } else {
             cout << "Seu chute foi maior que o número secreto!" << endl;
+            tentativas ++;
         }
     }
 
-    cout.precision(2); // Responsável por colocar precisão nas váriaveis numéricas do terminal 
-    cout << fixed; // Responsável por fixar as casas decimais no mesmo lugar
-    cout << "Sua pontuação foi de " << pontos << " pontos." << endl;
-    cout << "Fim de jogo!" << endl;
+    if (!acertou) {
+        cout << "\nVocê não acertou! Tente novamente!" << endl;
+    }
+    
+    cout << "\nFim de jogo!" << endl;
 }
