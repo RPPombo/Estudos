1770b5f58fa1dbdbf42eb72e63cad1ca93a423b8
RPPombo <rodrigopanisipombo@gmail.com>
2026-01-31 12:29:26 -0300

    Aprendendo a usar map, vector e refatorando o código

--- C++/Curso 1/JogoDaForca.cpp
+++ C++/Curso 1/JogoDaForca.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include <string>
-#include <cctype>
+#include <map>
+#include <vector>
 using namespace std;
 
 const string PALAVRA_SECRETA = "MELANCIA";
+map <char, bool> chutou; // O map é uma estrutura de dicionário, funcionando com chave/valor
+vector <char> chutes_errados; // O vector é um vetor dinâmico
 
 bool letra_existe(char chute) {
     for (char letra : PALAVRA_SECRETA) { // A partir do C++11 é possível iterar uma palavra da maneira como está no código ao invés de for (int i=0; i < PALAVRA_SECRETA.size(); i++)
@@ -15,25 +18,76 @@ bool letra_existe(char chute) {
     return false;
 }
 
-int main() {
-    bool acertou = false;
-    bool enforcou = false;
-
-    while (!acertou && !enforcou <= 5) {
-        char chute;
-
-        cout << "Faça seu chute: ";
-        cin >> chute;
-        chute = toupper(chute);
-
-        cout << "\nChute: " << chute << endl;
-
-        if (letra_existe(chute)) {
-            cout << "A letra existe na palavra" << endl;
-        } else {
-            cout << "A letra não existe na palavra" << endl;
+bool acertou_palavra() {
+    for (char letra : PALAVRA_SECRETA) {
+        if (!chutou[letra]) {
+            return false;
         }
     }
+
+    return true;
+}
+
+bool enforcou() {
+    return chutes_errados.size() >= 5;
+}
+
+void mostra_erros() {
+    cout << "Chutes errados: ";
+        for (char letra : chutes_errados) {
+            cout << letra << " ";
+        }
+    cout << endl;
+}
+
+void mostra_palavra() {
+    for (char letra : PALAVRA_SECRETA) {
+            if (chutou[letra]) {
+                cout << letra << " ";
+            } else {
+                cout << "_ ";
+            }
+        }
+    cout << endl;
+}
+
+void chuta() {
+    char chute;
+    cout << "Faça seu chute: ";
+    cin >> chute;
+
+    chutou[chute] = true;
+
+    cout << "\nChute: " << chute << endl;
+
+    if (letra_existe(chute)) {
+        cout << "A letra existe na palavra" << endl;
+    } else {
+        cout << "A letra não existe na palavra" << endl;
+        chutes_errados.push_back(chute);
+    }
+    cout << endl;
+}
+
+int main() {
+    cout << "Bem-vindo ao jogo da forca!" << endl;
+
+    while (!acertou_palavra() && !enforcou()) {
+        mostra_erros();
+
+        mostra_palavra();
+
+        chuta();
+    }
+    if (enforcou()) {
+        cout << "Que pena! Tente novamente!" << endl;
+    } else {
+        cout << "Parabéns! Você acertou a palavra secreta!" << endl;
+    }
+
+    cout << "Palavra secreta: " << PALAVRA_SECRETA << endl << endl;
+
+    cout<< "Fim de Jogo!" << endl;
 }
 
 // Para facilitar a compilação do arquivo, foi criado um arquivo Makefile.
