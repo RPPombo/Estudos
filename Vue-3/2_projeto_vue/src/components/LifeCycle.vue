74d8a12d62840a47e54ded003cb6b6a8f9c9f9b4
RPPombo <rodrigopanisipombo@gmail.com>
2026-05-29 16:06:46 -0300

    Começando estudos sobre o framework Vue (Vídeo 8)

---
+++ Vue-3/2_projeto_vue/src/components/LifeCycle.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h1>Meu nome é: {{ nome }}</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'LifeCycle',
+        data() {
+            return {
+                nome: "Ainda não sei"
+            }
+        },
+        created() {
+            setTimeout(() => {
+                this.nome = "Antonio"
+            }, 2000)
+        },
+        mounted() {
+            this.nome = "Carlos"
+        }
+    }
+</script>
---
+++ Vue-3/2_projeto_vue/src/components/Pessoa.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h2>Esta é a descrição da pessoa: {{ nome }}</h2> 
+        <p>Estou trabalhando no momento.</p>
+        <p>Sei as seguintes linguagens de programação:</p>
+        <ul>
+        <li>Python</li>
+            <li>C</li>
+            <li>Go</li>
+            <li>SQL</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Pessoa',
+        data() {
+            return {
+                nome: 'Rodrigo'
+            }
+        }
+    }
+</script>
